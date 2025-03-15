d3be4a316d25408bd6905073650b3df513747791
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-03-15 15:10:35 +0300

    Update linkNavigation.js

--- port_folio_react/portfolio/src/components/linkNavigation.js
+++ port_folio_react/portfolio/src/components/linkNavigation.js
@@ -1,13 +1,25 @@
+import { FaGithub, FaUniversity } from "react-icons/fa";
+
 function LinkNavigation() {
-    return(
-      <>
-      <h2>LINKS AND NAVIGATION</h2> 
-          <ul>
-            <li>🔗 <a href="https://github.com/MukamaJ-2" target="_blank" rel="noopener noreferrer">My GitHub Profile</a></li>
-            <li>📚 <a href="https://ucu.ac.ug/post-engineering-design-technology/" target="_blank" rel="noopener noreferrer">University-related webpage</a></li>
-          </ul>
-      </>
-    )
-    
-  }
-  export default LinkNavigation;
+  return (
+    <nav>
+      <h2>LINKS AND NAVIGATION</h2>
+      <ul>
+        <li>
+          <FaGithub />{" "}
+          <a href="https://github.com/MukamaJ-2" target="_blank" rel="noopener noreferrer">
+            My GitHub Profile
+          </a>
+        </li>
+        <li>
+          <FaUniversity />{" "}
+          <a href="https://ucu.ac.ug/post-engineering-design-technology/" target="_blank" rel="noopener noreferrer">
+            University-related webpage
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default LinkNavigation;
