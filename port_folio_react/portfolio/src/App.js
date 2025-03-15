8a9a8b20bbadd7004f3109938d642c5dd286c894
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-03-15 15:11:39 +0300

    Update App.js

--- port_folio_react/portfolio/src/App.js
+++ port_folio_react/portfolio/src/App.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
 import GeneralComponent from './components/generalComponent';
 import HobbyVideo from './components/hobbyVideo';
-import contactInformation from './components/contactInformation';
-import careerGoal from './components/careerGoals';
+import ContactInformation from './components/contactInformation';
+import CareerGoal from './components/careerGoals';
 import PersonalDetails from './components/personalDetails';
 import Profile from './components/profile';
 import LinkNavigation from './components/linkNavigation';
@@ -31,27 +31,29 @@ function App() {
   
   return (
     <>
+    
       <div className="theme-toggle">
         <button id="theme-switch" onClick={toggleTheme}>
           <span className="mode-icon">{isDarkMode ? '☀️' : '🌙'}</span>
           <span className="mode-text">{isDarkMode ? 'Light' : 'Dark'}</span>
         </button>
       </div>
-      <div className="container">
-        <hr />
-      </div>
+      
 
       <GeneralComponent
       heading="MUKAMA JOSEPH"
+      
       paragraph="I am a student at Uganda Christian University pursuing a Bachelor Degree in
     Computer Science. Web programming has been among my
     courses of interest cause before joining I had a dream of becoming a web developer
     and with this, I think it will contribute much to my success"/> 
-      <personalDetails/>
+      <PersonalDetails/>
       <Profile/>
       <HobbyVideo/>
       <ResultComponent/>
       <LinkNavigation/>
+      <ContactInformation/>
+      <CareerGoal/>
     </>
   )
 }
