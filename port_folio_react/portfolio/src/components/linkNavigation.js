e1d584e702363b1e9d07de40ff0a1a7f8e6f2640
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-03-15 13:54:31 +0300

    Create linkNavigation.js

---
+++ port_folio_react/portfolio/src/components/linkNavigation.js
@@ -0,0 +1,13 @@
+function LinkNavigation() {
+    return(
+      <>
+      <h2>LINKS AND NAVIGATION</h2> 
+          <ul>
+            <li>🔗 <a href="https://github.com/MukamaJ-2" target="_blank" rel="noopener noreferrer">My GitHub Profile</a></li>
+            <li>📚 <a href="https://ucu.ac.ug/post-engineering-design-technology/" target="_blank" rel="noopener noreferrer">University-related webpage</a></li>
+          </ul>
+      </>
+    )
+    
+  }
+  export default LinkNavigation;
