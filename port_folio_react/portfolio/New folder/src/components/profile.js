6fe45803cc22ec15aafb265f706e281f1519a6f6
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-03-15 15:12:44 +0300

    Create profile.js

---
+++ port_folio_react/portfolio/New folder/src/components/profile.js
@@ -0,0 +1,10 @@
+function Profile() {
+    return(
+      <>
+      <h2>PROFILE PICTURE</h2>
+          <img src="mukama.jpeg" alt="This is my profile pic" className="profile-image" />
+      </>
+    )
+  }
+
+  export default Profile
