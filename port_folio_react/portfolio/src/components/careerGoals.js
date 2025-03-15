2c2874c838669e654efb64226965b6592b210190
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-03-15 15:12:06 +0300

    Create careerGoals.js

---
+++ port_folio_react/portfolio/src/components/careerGoals.js
@@ -0,0 +1,20 @@
+function CareerGoal() {
+    return(
+      <>
+      <h2>CAREER GOALS</h2>
+          <div className="career-goals">
+            <p>
+              I want that at least by the end of this semester, I should be well equipped with
+              full-stack skills.
+            </p>
+            <p>
+              Next after obtaining all the required skills in full-stack development,
+              I would then like to add on Web3 skills that works with blockchains.
+            </p>
+          </div>
+          </>
+    )
+    
+  }
+
+  export default CareerGoal;
