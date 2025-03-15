7f32f00bbd1dcd2a2fc657213004e80dbf1d5b3e
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-03-15 13:53:56 +0300

    Create contactInformation.js

---
+++ port_folio_react/portfolio/src/components/contactInformation.js
@@ -0,0 +1,14 @@
+function contactInformation() {
+    return(
+    <>
+    <h2>CONTACT INFORMATION</h2>
+  <ul className="contact-info">
+    <li>📧 Email: <a href="mailto:josephmukama67@gmail.com">josephmukama67@gmail.com</a></li>
+    <li>💼 LinkedIn: <a href="https://www.linkedin.com/feed/" target="_blank" rel="noopener noreferrer">My LinkedIn Platform</a></li>
+    <li>💻 GitHub: <a href="https://github.com/MukamaJ-2" target="_blank" rel="noopener noreferrer">My GitHub Account</a></li>
+  </ul>
+    </>
+    )
+  }
+
+  export default contactInformation
