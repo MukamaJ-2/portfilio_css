2f5c0e2fb2ec93909f4432092ef965056637ebb2
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-03-15 15:11:13 +0300

    Create ResultsData.js

---
+++ port_folio_react/portfolio/src/util/ResultsData.js
@@ -0,0 +1,45 @@
+
+export const RESULTS =[
+    {
+        "id": 1,
+        "course_code": "ICT1102",
+        "course_title": "Essential Hardware and Software Concepts",
+        "score": 90,
+        "credit_units": 4
+    },
+    {
+        "id": 2,
+        "course_code": "ICT1103",
+        "course_title": "Fundamentals of Computing",
+        "score": 65,
+        "credit_units": 4
+    },
+    {
+        "id": 3,
+        "course_code": "CSC1101",
+        "course_title": "Structured Programming",
+        "score": 75,
+        "credit_units": 4
+    },
+    {
+        "id": 4,
+        "course_code": "MTH1102",
+        "course_title": "Discrete Mathematics",
+        "score": 70,
+        "credit_units": 3
+    },
+    {
+        "id": 5,
+        "course_code": "LNG1101",
+        "course_title": "Writing and Study Skills",
+        "score": 90,
+        "credit_units": 3
+    },
+    {
+        "id": 6,
+        "course_code": "TBS1103",
+        "course_title": "Understanding the Old Testament",
+        "score": 90,
+        "credit_units": 3
+    }
+]
