6c50574006996f1366ee00348c4aa95e58bef205
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-03-15 13:54:16 +0300

    Create heading.js

---
+++ port_folio_react/portfolio/src/components/heading.js
@@ -0,0 +1,16 @@
+function heading() {
+    return(
+      <>
+      <h1>MUKAMA JOSEPH</h1>
+  
+  <hr />
+  
+  <p>
+    I am a student at Uganda Christian University pursuing a Bachelor Degree in
+    Computer Science. Web programming has been among my
+    courses of interest cause before joining I had a dream of becoming a web developer
+    and with this, I think it will contribute much to my success
+  </p>
+      </>
+    )
+  }
